ec306bede5b8c135412cf5635efc6d2802bd86b7
JCKMTU <masahori713@gmail.com>
2017-09-11 09:36:51 +0800

    a

--- mobu-scripts/shell_command.py
+++
@@ -1,8 +0,0 @@
-import os
-from subprocess import *
-
-home_dir = os.path.expanduser("~")
-path = home_dir + "\Documents\hr_workspace\evenet-training-data\stage-3-train-ready\Anger\Ses01F_script01_Anger.pho"
-
-
-os.startfile(home_dir + "\Desktop\yeha.bat")
